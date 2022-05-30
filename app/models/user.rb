a7c78d9cde826669c73e7404cfe6967ab3086dbf
Alifudinniko <62735019+Alifudinniko@users.noreply.github.com>
2022-05-30 21:18:29 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true, format: { with:,  message:"Must be a valid email address"}
+    validates :email, presence: true, format: { with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i,  message:"Must be a valid email address"}
 end
 
