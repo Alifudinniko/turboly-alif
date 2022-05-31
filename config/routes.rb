5ea063fc8ca5c27bda5bbd10d0349b06e0bf6081
Alifudinniko <62735019+Alifudinniko@users.noreply.github.com>
2022-05-31 10:43:35 +0700

    Logging out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/tasks', to:'tasks#index'
+  get 'tasks', to:'tasks#index'
 
-  get '/sign_up',  to:'registrations#new'
-  post '/sign_in', to:'registrations#create'
+  get 'sign_up',  to:'registrations#new'
+  post 'sign_in', to:'registrations#create'
+  delete 'logout', to:'sessions#destroy'
 
   root to: "main#index"
 end
