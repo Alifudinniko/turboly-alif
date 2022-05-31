c70a953ab552115e01061331582c00b17fa9d3a2
Alifudinniko <62735019+Alifudinniko@users.noreply.github.com>
2022-05-31 11:32:18 +0700

    SIgn In
    +login form
    +accessing curend user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,8 +4,7 @@ class MainController < ApplicationController
         if session[:user_id]
             @user = User.find_by(id: session[:user_id])
         end
-       # flash.now[:notice] = "Logged in Successfult"
-       # flash.now[:alert] = "Invalid password or email"
+
     end
     
 end
